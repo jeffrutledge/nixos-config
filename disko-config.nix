b02a310583bbef38ef9911e13a8eb0bf48c1c1e2
Jeffrey Rutledge <jeff_rutledge@icloud.com>
2025-07-19 22:45:22 -0400

    added disko-config

---
+++ disko-config.nix
@@ -0,0 +1,70 @@
+{
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = "/dev/nvme0n1";
+        content = {
+          type = "gpt";
+          partitions = {
+            BOOT = {
+              size = "1G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            luksSwap = {
+              size = "100G";
+              content = {
+                type = "luks";
+                name = "luksSwap";
+                content = {
+                  type = "swap";
+		  resumeDevice = true;
+                };
+              };
+            };
+            luksRoot = {
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "luksRoot";
+                content = {
+                  type = "btrfs";
+                  extraArgs = [ "-f" ];
+                  subvolumes = {
+                    "/root" = {
+                      mountpoint = "/";
+                      mountOptions = [
+                        "compress=zstd"
+                        "noatime"
+                      ];
+                    };
+                    "/home" = {
+                      mountpoint = "/home";
+                      mountOptions = [
+                        "compress=zstd"
+                        "noatime"
+                      ];
+                    };
+                    "/nix" = {
+                      mountpoint = "/nix";
+                      mountOptions = [
+                        "compress=zstd"
+                        "noatime"
+                      ];
+                    };
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
