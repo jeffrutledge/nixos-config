5e8d3a5858a3393bf6dfad341c996ba0c527721a
Jeffrey Rutledge <misc@jeffrut.com>
2026-01-18 23:04:12 -0500

    added gds alias

--- home/git/aliases.nix
+++ home/git/aliases.nix
@@ -4,6 +4,7 @@
     glog = "git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative";
     gp = "git push origin HEAD";
     gd = "git diff";
+    gds = "git diff --staged";
     gc = "git commit -m";
     gs = "git status -sb";
     gac = "git add -A && git commit -m";
