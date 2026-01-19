a7d37f25fbef0d984a150f66c8c0b1157b9a8f8c
Jeffrey Rutledge <misc@jeffrut.com>
2026-01-19 15:25:38 -0500

    formated lua

--- home/neovim/init.lua
+++ home/neovim/init.lua
@@ -17,60 +17,60 @@ solarized_custom.inactive.b = { fg = "#586e75", bg = "#073642" }
 solarized_custom.inactive.c = { fg = "#586e75", bg = "#002b36" }
 
 require("lualine").setup({
-	options = {
-		theme = solarized_custom,
-		icons_enabled = true,
-	},
-	sections = {
-		lualine_a = { "mode" },
-		lualine_b = { "diagnostics" },
-		lualine_c = { "filename" },
-		lualine_x = { "filetype" },
-		lualine_y = { "progress" },
-		lualine_z = { "location" },
-	},
+  options = {
+    theme = solarized_custom,
+    icons_enabled = true,
+  },
+  sections = {
+    lualine_a = { "mode" },
+    lualine_b = { "diagnostics" },
+    lualine_c = { "filename" },
+    lualine_x = { "filetype" },
+    lualine_y = { "progress" },
+    lualine_z = { "location" },
+  },
 })
 
 -- blink.cmp completion
 require("blink.cmp").setup({
-	sources = {
-		default = { "lsp", "buffer", "snippets", "path" },
-		providers = {
-			buffer = {
-				opts = {
-					get_bufnrs = function()
-						return vim.tbl_filter(function(bufnr)
-							return vim.bo[bufnr].buftype == ""
-						end, vim.api.nvim_list_bufs())
-					end,
-				},
-			},
-		},
-	},
-	keymap = {
-		preset = "default",
-	},
-	completion = {
-		documentation = {
-			auto_show = true,
-		},
-	},
+  sources = {
+    default = { "lsp", "buffer", "snippets", "path" },
+    providers = {
+      buffer = {
+        opts = {
+          get_bufnrs = function()
+            return vim.tbl_filter(function(bufnr)
+              return vim.bo[bufnr].buftype == ""
+            end, vim.api.nvim_list_bufs())
+          end,
+        },
+      },
+    },
+  },
+  keymap = {
+    preset = "default",
+  },
+  completion = {
+    documentation = {
+      auto_show = true,
+    },
+  },
 })
 
 -- nvim-lint
 local lint = require("lint")
 
 lint.linters_by_ft = {
-	sh = { "shellcheck" },
-	bash = { "shellcheck" },
-	nix = { "statix" },
-	lua = { "luacheck" },
+  sh = { "shellcheck" },
+  bash = { "shellcheck" },
+  nix = { "statix" },
+  lua = { "luacheck" },
 }
 
 vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave", "BufEnter" }, {
-	callback = function()
-		vim.defer_fn(function()
-			lint.try_lint()
-		end, 100)
-	end,
+  callback = function()
+    vim.defer_fn(function()
+      lint.try_lint()
+    end, 100)
+  end,
 })
