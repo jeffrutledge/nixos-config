19a1974e6ad8f58ff5fe25edc862c5515dd513af
Jeffrey Rutledge <misc@jeffrut.com>
2026-01-19 15:10:47 -0500

    fixed low contrast in status line

--- home/neovim/init.lua
+++ home/neovim/init.lua
@@ -3,10 +3,22 @@ vim.opt.termguicolors = true
 vim.opt.background = "dark"
 vim.cmd.colorscheme("solarized")
 
--- Lualine statusline
+-- Lualine statusline with darker section b
+local solarized_custom = require("lualine.themes.solarized_dark")
+solarized_custom.normal.b = { fg = "#839496", bg = "#073642" }
+solarized_custom.normal.c = { fg = "#839496", bg = "#002b36" }
+solarized_custom.insert.b = { fg = "#839496", bg = "#073642" }
+solarized_custom.insert.c = { fg = "#839496", bg = "#002b36" }
+solarized_custom.visual.b = { fg = "#839496", bg = "#073642" }
+solarized_custom.visual.c = { fg = "#839496", bg = "#002b36" }
+solarized_custom.replace.b = { fg = "#839496", bg = "#073642" }
+solarized_custom.replace.c = { fg = "#839496", bg = "#002b36" }
+solarized_custom.inactive.b = { fg = "#586e75", bg = "#073642" }
+solarized_custom.inactive.c = { fg = "#586e75", bg = "#002b36" }
+
 require("lualine").setup({
 	options = {
-		theme = "solarized_dark",
+		theme = solarized_custom,
 		icons_enabled = true,
 	},
 	sections = {
