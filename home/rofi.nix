41c383c335c0ece62325bf82edcfaccea5595b81
Jeffrey Rutledge <misc@jeffrut.com>
2026-01-19 02:38:48 +0000

    changed rofi prompt and results to be visual separate

--- home/rofi.nix
+++ home/rofi.nix
@@ -25,16 +25,54 @@ in
         padding = mkLiteral "5px";
         border-color = mkLiteral c.blue;
       };
+      "mainbox" = {
+        spacing = mkLiteral "10px";
+        children = map mkLiteral [
+          "inputbar"
+          "listview"
+        ];
+      };
+      "inputbar" = {
+        children = map mkLiteral [
+          "prompt"
+          "entry"
+        ];
+        spacing = mkLiteral "10px";
+      };
+      "prompt" = {
+        background-color = mkLiteral c.blue;
+        text-color = mkLiteral c.base3;
+        padding = mkLiteral "2px 5px";
+      };
+      "entry" = {
+        background-color = mkLiteral c.base03;
+        text-color = mkLiteral c.base0;
+        padding = mkLiteral "2px 5px";
+      };
       "listview" = {
         lines = 10;
         columns = 1;
+        spacing = mkLiteral "2px";
       };
       "element" = {
         padding = mkLiteral "1px";
+        children = map mkLiteral [
+          "element-icon"
+          "element-text"
+        ];
       };
       "element selected" = {
         background-color = mkLiteral c.blue;
         text-color = mkLiteral c.base3;
+      };
+      "element-text" = {
+        background-color = mkLiteral "inherit";
+        text-color = mkLiteral "inherit";
+        vertical-align = mkLiteral "0.5";
+      };
+      "element-icon" = {
+        size = mkLiteral "1.0em";
+        background-color = mkLiteral "inherit";
       };
     };
   };
