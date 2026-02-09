c620fb52d69c9c96455f280c283caa57ba418dba
Jeffrey Rutledge <misc@jeffrut.com>
2026-02-08 21:21:36 -0500

    change picker to use project root of current buffer

--- home/neovim/lua/plugins/snacks.lua
+++ home/neovim/lua/plugins/snacks.lua
@@ -1,12 +1,16 @@
+local function get_project_root()
+  return vim.fs.root(0, { ".git", "flake.nix" }) or vim.fn.getcwd()
+end
+
 require("snacks").setup({
   picker = {},
 })
 
 vim.keymap.set("n", "<leader>ff", function()
-  Snacks.picker.files()
+  Snacks.picker.files({ cwd = get_project_root() })
 end, { desc = "Find files" })
 vim.keymap.set("n", "<leader>fg", function()
-  Snacks.picker.grep()
+  Snacks.picker.grep({ cwd = get_project_root() })
 end, { desc = "Grep" })
 vim.keymap.set("n", "<leader>fl", function()
   Snacks.picker.lines()
