6ae4dab97ec89a88433e98d4166dcc3125ef43bb
Jeffrey Rutledge <misc@jeffrut.com>
2026-02-14 16:41:41 -0500

    change stylua indent config to match vim indent lua cfg

--- home/neovim/lua/plugins/conform.lua
+++ home/neovim/lua/plugins/conform.lua
@@ -3,6 +3,11 @@ require("conform").setup({
     lua = { "stylua" },
     nix = { "nixfmt" },
   },
+  formatters = {
+    stylua = {
+      prepend_args = { "--indent-type", "Spaces", "--indent-width", "2" },
+    },
+  },
   format_on_save = {
     timeout_ms = 500,
     lsp_format = "fallback",
