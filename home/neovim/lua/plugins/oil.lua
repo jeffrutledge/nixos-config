0a4e20a0be294819788bec81ea3377d793b2f22e
Jeffrey Rutledge <misc@jeffrut.com>
2026-02-14 18:48:14 -0500

    enable git actions in oil

--- home/neovim/lua/plugins/oil.lua
+++ home/neovim/lua/plugins/oil.lua
@@ -4,6 +4,17 @@ require("oil").setup({
     "size",
     { "mtime", "%Y-%m-%d %H:%M" },
   },
+  git = {
+    add = function(path)
+      return true
+    end,
+    mv = function(src_path, dest_path)
+      return true
+    end,
+    rm = function(path)
+      return true
+    end,
+  },
 })
 
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
