1559e0bacf9f381376cfde8b85c277f583c3df2d
Jeffrey Rutledge <misc@jeffrut.com>
2026-02-15 13:05:49 -0500

    changed starship path truncate settings

--- home/starship.nix
+++ home/starship.nix
@@ -3,5 +3,10 @@
   programs.starship = {
     enable = true;
     enableZshIntegration = true;
+    settings = {
+      directory = {
+        fish_style_pwd_dir_length = 1;
+      };
+    };
   };
 }
