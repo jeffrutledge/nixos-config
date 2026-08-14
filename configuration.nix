2d3d74979cf312ca67a5278a7557e2520446c11a
Jeffrey Rutledge <misc@jeffrut.com>
2026-08-14 16:15:42 -0600

    added memtest86 to boot

--- configuration.nix
+++ configuration.nix
@@ -6,7 +6,10 @@
 {
   boot.loader = {
     efi.canTouchEfiVariables = true;
-    systemd-boot.enable = true;
+    systemd-boot = {
+      enable = true;
+      memtest86.enable = true;
+    };
   };
 
   networking = {
